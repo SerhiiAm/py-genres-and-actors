f1cf39586a5c307b2698042a1b2d3f8e1373ac58
Serhii <sergei.amvarcumov@gmail.com>
2026-04-09 21:56:27 +0200

    Fix: remove manual cleanup

--- main.py
+++ main.py
@@ -6,33 +6,38 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.all().delete()
-    Actor.objects.all().delete()
+
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
     Genre.objects.create(name="Dramma")
+
     Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
     Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
-    # --- UPDATE ---
+
+    # 2. UPDATE
     genre_dramma = Genre.objects.get(name="Dramma")
     genre_dramma.name = "Drama"
     genre_dramma.save()
+
     actor_klooney = Actor.objects.get(first_name="George", last_name="Klooney")
     actor_klooney.last_name = "Clooney"
     actor_klooney.save()
+
     actor_reaves = Actor.objects.get(first_name="Kianu", last_name="Reaves")
     actor_reaves.first_name = "Keanu"
     actor_reaves.last_name = "Reeves"
     actor_reaves.save()
+
     # 3. DELETE
     Genre.objects.get(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
+    # 4. RETURN
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 if __name__ == "__main__":
     print(main())
